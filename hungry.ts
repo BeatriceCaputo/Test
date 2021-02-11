789033e8208bf2cb7cfbb7dfc6d2875dc59d3c2d
CaBeat <bcaputo92@gmail.com>
2021-02-11 20:22:41 +0100

    new feature branch, add features to hungry.ts

--- hungry.ts
+++ hungry.ts
@@ -1,5 +1,16 @@
 let myFirstName : string = "Beatrice";
 let myLastName : string = "Caputo";
-let myId : int = 134503;
+let myId : number = 134503;
 
-console.log(myvar);
+console.log(myFirstName);
+
+/**
+ * The following are the new feature
+ * To add a new feature I'm going to modify this file;
+ * then I will create a new one
+ */
+let myArray : string[] = ["mum", "dad", "sister"];
+let i : number;
+for (i = 0; i<myArray.length; i++ ){
+    console.log(myArray[i]);
+}
